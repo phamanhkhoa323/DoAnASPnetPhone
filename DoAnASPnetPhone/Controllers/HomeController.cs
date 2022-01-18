d7746bfccdc9c3684fe0de165d06957aed02992c
phamanhkhoa323 <93703554+phamanhkhoa323@users.noreply.github.com>
2022-01-18 11:03:30 +0700

    Add project files.

---
+++ DoAnASPnetPhone/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using DoAnASPnetPhone.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace DoAnASPnetPhone.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Index()
+        { /*if(HttpContext.Request.Cookies.ContainsKey("AccountUsername"))
+            {
+                ViewBag.AccountUsername = HttpContext.Request.Cookies["AccountUsername"].ToString(); 
+            }  */
+            if (HttpContext.Session.Keys.Contains("NguoidungEmail"))
+            {
+                ViewBag.AccountUsername = HttpContext.Session.GetString("NguoidungEmail");
+            }
+            return View();
+        }
+    }
+}
