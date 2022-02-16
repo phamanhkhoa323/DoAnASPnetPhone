e0ddfd73bba111cc103778c7a0472a7d62b8c1be
DELL <DELL@DESKTOP-CG3KOCH>
2022-02-16 15:28:32 +0700

    dashboard

--- DoAnASPnetPhone/Startup.cs
+++ DoAnASPnetPhone/Startup.cs
@@ -59,6 +59,13 @@ namespace DoAnASPnetPhone
 
             app.UseEndpoints(endpoints =>
             {
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllerRoute(
+                      name: "areas",
+                      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                    );
+                });
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
